da1767e6b369b2cb674d7717eaf3481f7d481c30
Kevin Roberts <kevin@shoutly.com>
2023-05-08 15:40:40 +0200

    Add test-results into circleci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,14 @@
 import { PlaywrightTestConfig } from '@playwright/test'
 import * as dotenv from 'dotenv'
 dotenv.config()
+import path from 'path'
+
+// Load environment configuration based on the ENVIRONMENT environment variable
+const env = process.env.ENVIRONMENT || 'test'
+dotenv.config({ path: path.resolve(__dirname, `.env.${env}`) })
+
+// Print the environment name to the console
+console.log(`Executing tests in the '${env}' environment.`)
 
 const config: PlaywrightTestConfig = {
 	use: {
@@ -9,8 +17,8 @@ const config: PlaywrightTestConfig = {
 		ignoreHTTPSErrors: true,
 	},
 	workers: 1,
-	timeout: 80000,
-	retries: 0,
+	timeout: parseInt(process.env.TIMEOUT ?? '30000'),
+	retries: parseInt(process.env.RETRIES ?? '0'),
 	expect: { timeout: 25000 },
 }
 export default config
